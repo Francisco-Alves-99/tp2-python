1f06a22edb785f8e7bbe5053e50d9ea92077cbcb
Francisco Alves <franciscoalves@MacBook-Air-de-Francisco.local>
2024-11-10 18:26:52 -0300

    ex13

---
+++ ex13.py
@@ -0,0 +1,6 @@
+texto_usuario = input("Digite o texto desejado: ") # Recebe o texto do usuario
+
+def printa_texto(texto):
+    print(texto) # Faz o print do texto
+
+printa_texto(texto_usuario) # Chamada da funcao   
