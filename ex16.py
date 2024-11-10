79c67ea69aae1dbef98459bf8cc1a8967b5ce5dc
Francisco Alves <franciscoalves@MacBook-Air-de-Francisco.local>
2024-11-10 18:41:18 -0300

    ex16

---
+++ ex16.py
@@ -0,0 +1,4 @@
+def calcular_total(preco, quantidade=1):
+    return preco * quantidade # retorna o resultado da operacao do preço multiplicado pela quantidade
+
+print(calcular_total(5, 2)) # chamada da funcao
