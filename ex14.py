b0644705431ba1dfe1b94249caa5892e5845a979
Francisco Alves <franciscoalves@MacBook-Air-de-Francisco.local>
2024-11-10 18:32:37 -0300

    ex14

---
+++ ex14.py
@@ -0,0 +1,13 @@
+def dividir(num1, num2):
+    divisao_inteira = num1 // num2
+    resto = num1 % num2
+
+    return divisao_inteira, resto
+
+print(dividir(5, 2))
+
+
+''''
+a funcao acima recebe dos valores numéricos como parametros e, faz a divisao inteira dos valores, assim como o resto da mesma.
+após as operacoes, ela retorna os resultados.
+'''
